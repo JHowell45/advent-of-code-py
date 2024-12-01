d4e307378cbe8bfc2a0179f1e40a5ca07970bce4
= <jhowell45@googlemail.com>
2024-12-01 16:09:53 +0000

    adding command for running the solutions

--- event_2024/__init__.py
+++ event_2024/__init__.py
@@ -1,3 +1,22 @@
+import click
+
 from src.day_1.part_a import part_a
 
-part_a()
+
+@click.command()
+@click.argument("day", type=int)
+@click.argument("part", type=click.Choice(["a", "b"]))
+def aoc(day: int, part: str):
+    match day:
+        case 1:
+            match part:
+                case "a":
+                    part_a()
+                case "b":
+                    pass
+        case _:
+            print(f"Day {day} has not yet been implemented!")
+
+
+if __name__ == "__main__":
+    aoc()
