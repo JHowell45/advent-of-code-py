831e0a31a9a26d8b9b9e5f2705cf82fc12a7dc1f
= <jhowell45@googlemail.com>
2024-12-01 15:40:44 +0000

    adding basic model for loading the search class instance.

--- 2024/src/day_1/shared.py
+++ 2024/src/day_1/shared.py
@@ -1,5 +1,24 @@
+from __future__ import annotations
+
 from pydantic import BaseModel
 
 
 class LocationSearch(BaseModel):
-    pass
+    left: list[int]
+    right: list[int]
+
+    def add_left(self, index: int, value: int):
+        self.left[index] = value
+
+    def add_right(self, index: int, value: int):
+        self.right[index] = value
+
+    @classmethod
+    def parse_locations(cls, data: str) -> LocationSearch:
+        instance = LocationSearch(left=[0] * 1000, right=[0] * 1000)
+        for index, line in enumerate(data.splitlines()):
+            left, right = line.split("   ")
+            instance.add_left(index, int(left))
+            instance.add_right(index, int(right))
+
+        return instance
