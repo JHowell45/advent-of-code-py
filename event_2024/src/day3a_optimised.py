0e2eccd301cb709c5cb0e42ad14f89a5c0f1be87
= <jhowell45@googlemail.com>
2024-12-03 19:38:07 +0000

    attempting to optimise

---
+++ event_2024/src/day3a_optimised.py
@@ -0,0 +1,15 @@
+from re import finditer
+
+from core.file_reader import get_file
+
+@get_file(3)
+def main(file):
+    total = 0
+    pattern: str = "(mul\\((?P<a>\\d{1,3}),(?P<b>\\d{1,3})\\))"
+    for line in finditer(pattern, file.read()):
+        total += int(line.group('a')) * int(line.group('b'))
+    print(f"Total multiplications: {total}")
+
+
+if __name__ == "__main__":
+    main()
